0bc798217c7b3d36089bf0c44896ad06118ba5b2
saigegit <saige.genetics@gmail.com>
2022-02-13 15:51:40 -0500

    update dockerfile

---
+++ docker/install_packages.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript
+#install required R packages, from Finnge/SAIGE-IT
+
+req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "MetaSKAT", "roxygen2", "rversions","devtools")
+for (pack in req_packages) {
+    if(!require(pack, character.only = TRUE)) {
+        install.packages(pack, repos = "https://cloud.r-project.org")
+    }
+}
