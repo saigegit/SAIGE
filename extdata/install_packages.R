1768dd9a481b2bca08eb67f9bb0a20a8c46df5a6
saigegit <saige.genetics@gmail.com>
2022-02-28 10:32:08 -0500

    configure

--- extdata/install_packages.R
+++ extdata/install_packages.R
@@ -9,8 +9,10 @@ for (pack in req_packages) {
 	print(packageVersion(pack))
     }
 }
+devtools::install_version("Rcpp", version = "1.0.7", repos = "http://cran.us.r-project.org")
+print(packageVersion("Rcpp"))
 install.packages("RSQLite", repos='http://cran.rstudio.com/')
-print(packageVersion(RSQLite))
+print(packageVersion("RSQLite"))
 #devtools::install_github("leeshawn/SPAtest")
 #devtools::install_github("leeshawn/MetaSKAT")
 #devtools::install_github("leeshawn/SKAT")
