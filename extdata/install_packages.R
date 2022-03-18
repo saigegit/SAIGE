7bead7be1d602c26146cda4211ba94af45f89bd2
wzhou <zhowei@umich.edu>
2022-03-18 03:33:46 +0000

    install 0.10.7.5.0

--- extdata/install_packages.R
+++ extdata/install_packages.R
@@ -8,13 +8,19 @@ install.packages("vctrs", repos='http://cran.rstudio.com/')
 print(packageVersion("vctrs"))
 
 
-req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "roxygen2", "rversions","devtools", "SKAT", "RhpcBLASctl", "qlcMatrix", "dplyr", "dbplyr")
+req_packages <- c("R.utils", "Rcpp", "RcppParallel", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "roxygen2", "rversions","devtools", "SKAT", "RhpcBLASctl", "qlcMatrix", "dplyr", "dbplyr")
 for (pack in req_packages) {
     if(!require(pack, character.only = TRUE)) {
         install.packages(pack, repos = "https://cloud.r-project.org", dependencies=TRUE)
         print(packageVersion(pack))
     }
 }
+
+if((!require("RcppArmadillo", character.only = TRUE)) | packageVersion("RcppArmadillo") < "0.10.7.5.0"){
+    install.packages("RcppArmadillo", repos = "https://cloud.r-project.org", dependencies=TRUE)
+}
+
+
 devtools::install_version("Rcpp", version = "1.0.7", repos = "http://cran.us.r-project.org")
 print(packageVersion("Rcpp"))
 install.packages("RSQLite", repos='http://cran.rstudio.com/')
