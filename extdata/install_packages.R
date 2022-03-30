2030bb30756d55af72f571bb908ffb5c98ca2117
Alex Petty <pettyalex@gmail.com>
2022-03-30 11:32:24 -0500

    Move to a simplified Dockerfile that uses ubuntu distributed R. Still links against openBLAS

--- extdata/install_packages.R
+++ extdata/install_packages.R
@@ -8,18 +8,23 @@ install.packages("vctrs", repos='http://cran.rstudio.com/')
 print(packageVersion("vctrs"))
 
 
-req_packages <- c("R.utils", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "roxygen2", "rversions","devtools", "SKAT", "RhpcBLASctl", "qlcMatrix", "dplyr", "dbplyr")
+req_packages <- c("R.utils", "devtools", "Rcpp", "RcppParallel", "RcppArmadillo", "data.table", "RcppEigen", "Matrix", "methods", "BH", "optparse", "SPAtest", "roxygen2", "rversions","devtools", "SKAT", "RhpcBLASctl", "qlcMatrix", "dplyr", "dbplyr")
 for (pack in req_packages) {
     if(!require(pack, character.only = TRUE)) {
         install.packages(pack, repos = "https://cloud.r-project.org", dependencies=TRUE)
         print(packageVersion(pack))
     }
 }
-devtools::install_version("Rcpp", version = "1.0.7", repos = "http://cran.us.r-project.org")
+
+github_packages <- c("leeshawn/MetaSKAT")
+for (pack in github_packages) {
+    if(!require(pack, character.only = TRUE)) {
+        devtools::install_github(pack, repos = "https://cloud.r-project.org")
+    }
+}
+
+# devtools::install_version("Rcpp", version = "1.0.7", repos = "http://cran.us.r-project.org")
 print(packageVersion("Rcpp"))
 install.packages("RSQLite", repos='http://cran.rstudio.com/')
 print(packageVersion("RSQLite"))
 
-#devtools::install_github("leeshawn/SPAtest")
-#devtools::install_github("leeshawn/MetaSKAT")
-#devtools::install_github("leeshawn/SKAT")
