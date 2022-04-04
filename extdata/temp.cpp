00440ed09558314c4e150bf24439f71a3b9fbbea
wzhou <zhowei@umich.edu>
2022-04-04 03:23:30 +0000

    clean

--- extdata/temp.cpp
+++
@@ -1,4 +0,0 @@
-        Unified_getMarkerPval(
-	                    GVec,
-			                        false, // bool t_isOnlyOutputNonZero,
-						          indexNonZeroVec_arma, indexZeroVec_arma, Beta, seBeta, pval, pval_noSPA, Tstat, gy, varT, altFreq, isSPAConverge, gtildeVec, is_gtilde, true, P2Vec, isCondition, Beta_c, seBeta_c, pval_c, pval_noSPA_c, Tstat_c, varT_c, G1tilde_P_G2tilde_Vec, is_Firth, is_FirthConverge);
